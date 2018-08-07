c3ca62b5f015a6c874d77525ad20bdc4ef997948
yifan <wyifan@live.cn>
2018-08-07 10:13:11 +0800

    Add project files.

---
+++ GithubDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test adding exist solution to github.");
+        }
+    }
+}
